5fb2526840a4912ff1d66f0f93a0e428542c225a
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2014-11-24 11:21:56 -0200

    Initial commit
    empty project

---
+++ lib/spot/parser.rb
@@ -0,0 +1,7 @@
+require "spot/parser/version"
+
+module Spot
+  module Parser
+    # Your code goes here...
+  end
+end
---
+++ lib/spot/parser/version.rb
@@ -0,0 +1,5 @@
+module Spot
+  module Parser
+    VERSION = "0.0.1"
+  end
+end
